f8046167f71b3c0925f1696ac904e1e84a855175
Johnathan <Johnathan2@outlook.com.br>
2021-05-23 12:57:42 -0300

    if composto e encadeado desenvolvidos

--- src/TestandoS1.java
+++ src/TestandoS1.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 public class TestandoS1 {
 
 	public static void main(String[] args) {
+		//*uma ONG criou um jogo para alertar as pessoa sobre os riscos das drogas.
+		//Por se tratar de um assunto sensível, ficou decidido que a idade mínima para jogar é de 12 anos.
+		//Sua função é criar um programa que receba a idade do usuário e exiba a mensagem "Você pode jogar" caso ele tenha 12 anos ou mais. */
 		Scanner leitor = new Scanner(System.in);
-		
+	
 		int idade;
-		System.out.println("Por favor, digita sua idade");
+		System.out.println("Por favor, digite sua idade");
 		idade = leitor.nextInt();
-		System.out.println("Deixe-me , verifica sua idade");
-		if (idade>=12) {
+		
+		System.out.println("Deixe-me, verifica sua idade");
+		if (idade>=12)	{
 			
-		System.out.println("Você pode jogar");
+			
+		System.out.println("Você pode jogar!");
 		Scanner leitor1 = new Scanner(System.in);
 		int ano;
-		System.out.println("Por favor, digita a data do seu nascimento");
+		System.out.println("Por favor , digite a data do seu nascimento");
 		ano = leitor.nextInt();
-		
+		System.out.println("Que legal, você nasceu em " + ano);
 		idade = 2021 - ano;
-		System.out.println("Pelo que eu vi , sua idade é + idade");
-		if (idade>=18) {
+		System.out.println("Pelo que eu vi , você nasceu em " + idade);
+		
+		
 			
+
 
 		
 		
@@ -28,11 +35,11 @@ public class TestandoS1 {
 		leitor.close();
 		
 		
-		
 		}
 		
 	}
-
-	}
 	
 }
+
+	
+

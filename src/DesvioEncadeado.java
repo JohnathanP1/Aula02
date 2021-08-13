359b495b957565a077c2703fe3e732977a1cdbb9
Johnathan <Johnathan2@outlook.com.br>
2021-08-13 20:30:12 -0300

    If composto encadeado desenvolvidos

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -2,51 +2,46 @@ import java.util.Scanner;
 
 public class DesvioEncadeado {
 
-	public static void main(String[] args) {
-		/*
-		 * Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para
-		 * buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem
-		 * votar. Para evitar problemas no momento do embarque, porém, você foi
-		 * convidado a criar um programa que valide a idade dos passageiros: caso tenham
-		 * 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos
-		 * e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos,
-		 * devem votar obrigatoriamente. Crie um programa que receba a idade dos
-		 * passageiros em potencial e indique se podem embarcar e votar.
-		 */
 
+	public static void main(String[] args) {
+		/*Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem votar.
+		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
+		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
+		
 		Scanner leitor = new Scanner(System.in);
 		int idade;
 		System.out.println("Por favor, digite a idade do passageiro.");
 		idade = leitor.nextInt();
-
-		/* Aqui devemos criar uma solução para realizar as verificações. */
-		/*
-		 * if (idade <16) {
-		 * System.out.println("Essa pessoa não pode votar e não pode embarcar"); idade =
-		 * leitor.nextInt(); }else { if (idade <18) {
-		 * 
-		 * System.out.
-		 * println("Tanto a votação quanto o embarque são opcionais para essa pessoa");
-		 * idade = leitor.nextInt();
-		 * 
-		 * }else { System.out.println("Essa pessoa é obrigada a votar"); }
-		 */
-
-		//
-		if (idade <=16) {
+	
+		/*Aqui devemos criar uma solução para realizar as verificações.*/
+		/*if (idade <16) {
 			System.out.println("Essa pessoa não pode votar e não pode embarcar");
-		//Desempenho é a mesma coisa, mais else if facilita fica legivél
-		} else if (idade < 18) {
-			System.out.println("Tanto a votação quanto o embarque são opcionais para essa pessoa");
-
-		} else {
-			System.out.println("Essa pessoa é obrigada a votar");
-
+			idade = leitor.nextInt();
+		}else {
+			if (idade <18) {
 			
-		//Depois commita e envia no github	
+			System.out.println("Tanto a votação quanto o embarque são opcionais para essa pessoa");
+			idade = leitor.nextInt();
+			
+			}else {
+				System.out.println("Essa pessoa é obrigada a votar");
+				}*/
+			
+		if(idade <=16) {
+			System.out.println("Essa pessoa não pode votar e não pode embarcar");
+		}else if (idade <18) {
+			System.out.println("Tanto a votação quanto o embarque são opcionais para essa pessoa");
+		
+		}else {
+			System.out.println("Essa pessoa é obrigada a votar");
+	
+		
 			
 			leitor.close();
 		}
 	}
-
+	
 }
+	
+
+
